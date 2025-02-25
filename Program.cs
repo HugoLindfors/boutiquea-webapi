287202ff8fbb6143fcb53cbc24b7ba88530783fb
Hugo Lindfors <hugo.lindfors2@outlook.com>
2025-02-25 16:03:58 +0100

    Getting started, created models, created WORKFLOW.md

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
+using Boutiquea.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +13,40 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/api/customers", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
+    var customers = Enumerable.Range(1, 5).Select(index =>
+        new Customer
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+
         ))
         .ToArray();
-    return forecast;
+    return customers;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetCustomers");
+
+app.MapGet("/api/orders", () =>
+{
+    var orders = Enumerable.Range(1, 5).Select(index =>
+        new Order
+        (
+
+        ))
+        .ToArray();
+    return orders;
+})
+.WithName("GetOrders");
+
+app.MapGet("/api/products", () =>
+{
+    var products = Enumerable.Range(1, 5).Select(index =>
+        new Product
+        (
+
+        ))
+        .ToArray();
+    return products;
+})
+.WithName("GetProducts");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ WORKFLOW.md
@@ -0,0 +1,5 @@
+# Here i try to explain what I'm doing!
+
+I thought it was a good idea to start with the Models. I need a Model for Order, Product and Customer.
+
+An Order contains some kind of Array or List of Products as well as a total Price. In this assignment we don't have to add some kind of login system or passwd auth or so.
