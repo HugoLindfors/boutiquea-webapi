0ac64f9bba62b55efff3c2b393cb815bec8500be
Hugo Lindfors <hugo.lindfors2@outlook.com>
2025-02-27 21:00:05 +0100

    Better README.md - Set Up added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Boutiquea Web API
 Boutiquea is a store system developed for Ungdomskraft AB as part of the recruitment process; this is the web API/back end developed using .NET.
+
+# Set Up
+Make sure to have .NET 9.0 installed
+
+Install repo
+
+Enter repo
+
+Type `dotnet run`
+
+The Web API can run without the Front End, but it's kinda useless on its own
+
+Make sure to have NUXT installed
+
+Install the Front End repo
+
+Enter repo
+
+Type `npm install`
+
+Type `npm run dev`
--- obj/Debug/net9.0/Boutiquea.AssemblyInfo.cs
+++ obj/Debug/net9.0/Boutiquea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f77eacf2b4035a0511c3e66e134a2285831edc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c520513467ed256dbc3e25aae957e5bfe462ea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
