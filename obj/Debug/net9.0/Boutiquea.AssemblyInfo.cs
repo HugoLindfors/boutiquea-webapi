0c520513467ed256dbc3e25aae957e5bfe462ea7
Hugo Lindfors <hugo.lindfors2@outlook.com>
2025-02-27 15:14:36 +0100

    Reverted line comments

--- obj/Debug/net9.0/Boutiquea.AssemblyInfo.cs
+++ obj/Debug/net9.0/Boutiquea.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd286d7ff453ac801e1be5a034895758bf8ebc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f77eacf2b4035a0511c3e66e134a2285831edc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Boutiquea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
